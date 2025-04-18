cc1e3e34aa6064179da14d73695ef7fe95eb6efc
Ne-x-tr-on <26nextron.dev@gmail.com>
2025-04-18 14:36:29 +0300

    Nodding

--- devpackages/index.js
+++ devpackages/index.js
@@ -2,3 +2,21 @@
 //Keeps on spawning
 
 console.log('Nextron only codes in Rust');
+
+const {format}= require('date-fns');
+console.log(format(new Date(),'yyyyMMdd\tHH:M:SS'));
+const {v4: uuid} = require('uuid');
+console.log(uuid());
+ c
+const logEvent = require('./logEvents');
+const EventEmmitter = require('events');
+
+class MyEmitter extends EventEmmitter{};
+
+const myEmitter = new MyEmitter();
+
+myEmitter.on('log',(msg) =>logEvent(msg));
+
+setTimeout(()=>{
+  myEmitter.emit('log','log event Emited!');
+},2000);
